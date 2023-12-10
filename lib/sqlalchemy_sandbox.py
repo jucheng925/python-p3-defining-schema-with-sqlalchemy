be5585cc4378000d21ee41ecc72e8e8d96ef2a55
Julie Cheng <jucheng925@gmail.com>
2023-12-10 10:58:03 -0500

    code along

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -6,7 +6,17 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Student(Base):
-    pass
+    __tablename__ = 'students'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
 
 if __name__ == '__main__':
-    pass
+    engine = create_engine('sqlite:///students.db')
+    Base.metadata.create_all(engine)
+
+'''
+The engine is "home base" for the database- everything on the database side and the Python side must pass through the engine for the process to count. Here, we're pointing to a local sqlite file where our tables will be created.
+
+The create_all() command on the next line tells the engine that any models that were created using Base as a parent should be used to create tables.
+'''
